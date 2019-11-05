2fb989c985a01c947d10684b6ac728211516682d
bakudan00 <52767674+bakudan00@users.noreply.github.com>
2019-11-05 17:11:36 +0800

    showed name from url parameter in template

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<name>')
+def index(name):
+    return render_template('index.html', name=name)
 
 @app.route('/Home')
 def home():
