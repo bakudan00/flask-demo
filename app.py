b9903c455682d7c21f155c28744748eec84b61de
bakudan00 <52767674+bakudan00@users.noreply.github.com>
2019-11-06 10:14:45 +0800

    added link on homepage to go to contact page

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # signed_in = True
     return render_template('index.html')
 
 @app.route('/contact')
-def home():
+def contact():
+    # signed_in = True
     return render_template('contact.html')
 
 if __name__ == '__main__':
