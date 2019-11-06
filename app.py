5129fb912f9eff00c6a6123d0579aa1f833ef306
bakudan00 <52767674+bakudan00@users.noreply.github.com>
2019-11-06 12:56:06 +0800

    added non-functioning contact us form

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     # signed_in = True
+    first_name = request.args.get("first_name")
     return render_template('index.html')
 
 @app.route('/contact')
